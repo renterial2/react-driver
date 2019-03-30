e0b6f63382f38a9f9f3d7ec4091ea4076ba8a3c9
RenteriaL <renterial@thesharepointprofessional.com>
2019-03-30 17:54:57 -0500

    attempting to turn off SM2's debugMode

--- src/components/LevelMusic.jsx
+++ src/components/LevelMusic.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Sound from 'react-sound'
 import soundfile from '../sfx/background.mp3'
+import 'react-sound'
+soundManager.setup({debugMode: false})
 
 class LevelMusic extends Component {
 
