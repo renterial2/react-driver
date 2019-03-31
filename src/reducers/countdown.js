1119cb4ce22ca7100e0986d5544aef8025a7906e
RenteriaL <renterial@thesharepointprofessional.com>
2019-03-30 23:26:13 -0500

    decrement the Countdown timer

--- src/reducers/countdown.js
+++ src/reducers/countdown.js
@@ -1,17 +1,12 @@
 function countdown (state, action) {
     if (!state.gamestate.started) return state
+    let gametime = gamestate.time
+    const time = gametime - 1
 
-    setInterval(caculateTimeRemaining, 1000)
-
-    function caculateTimeRemaining() {
-        let gametime = gamestate.time
-        const time = gametime - 1
-
-        return {
-            ...newState,
-            gamestate: {
-                time,
-            }
+    return {
+        ...state,
+        gamestate: {
+            time,
         }
     }
 }
